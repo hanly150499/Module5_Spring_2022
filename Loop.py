224b5ebcacd1251958bcedce02dc283fbce91924
lymyh <hly1@dmacc.edu>
2022-02-19 22:07:49 -0600

    Intital Commit

---
+++ Loop.py
@@ -0,0 +1,15 @@
+"""Basic for Loops Assignment
+In the editor of your choice, write a Python script (.py file)
+Declare a list of floating point numbers
+Write a for loop to print each
+Write a second for loop to print the odd number descending from 99 to 33 (including 99 and 33)
+Include in .py file a docstring."""
+
+float = [5.5,4.5,8.5,6.3]
+for x in float:
+    print(x)
+for y in range(99,32,-2):
+    print(y)
+
+
+
